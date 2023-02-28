dde533aaf9f8159292cf06c0783cb419a3b701ba
Alina Revutska <alinkarevutska@gmail.com>
2023-02-28 17:09:53 +0000

    displaying the list, removing a book

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import {useState} from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList'
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
 
   const createBook = (title) => {
     console.log('need to add book with title:', title);
@@ -16,7 +25,8 @@ function App() {
   }
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById}/>
       <BookCreate onCreate={createBook}/>
     </div>
   );
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import BookShow from './BookShow';
 
-const BookList = () => {
+const BookList = ({books, onDelete}) => {
+    const renderedBooks = books.map((book) => {
+        return <BookShow key={book.id} book={book}
+        onDelete={onDelete}/>
+    });
+
     return (
-        <div>
-           BookList 
+        <div className="book-list">
+            {renderedBooks}
         </div>
     );
 };
