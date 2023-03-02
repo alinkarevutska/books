30a3204026f156d60f03f1966c86f5682c7b4cb3
Alina Revutska <alinkarevutska@gmail.com>
2023-03-02 15:46:06 +0000

    added fetching, creating, updating, deleting books wia axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
-import BookList from './components/BookList'
+import BookList from './components/BookList';
+
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const deleteBookById = (id) => {
+  const fetchBooks = async () => {
+    const responce = await axios.get('http://localhost:3001/books');
+    setBooks(responce.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -14,22 +27,28 @@ function App() {
   };
 
   // finding a particular book in the books object and editing it
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const responce = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+
     const editedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle}
+        return { ...book, ...responce.data} // we do not update parts of book object manually - we take all data from the responce and put it to our editedBooks piece of state
       };
       return book;
     });
     setBooks(editedBooks);
   };
 
-  const createBook = (title) => {
-    console.log('need to add book with title:', title);
+  const createBook = async (title) => {
+    const responce = await axios.post('http://localhost:3001/books', {
+      title: title
+    });
+
     const updatedBooks = [
       ...books,
-      { id: Math.round(Math.random() * 9999), 
-        title: title }
+      responce.data // adding a title from the responce to a updatedBooks array
     ];
     setBooks(updatedBooks);
     console.log(updatedBooks);
